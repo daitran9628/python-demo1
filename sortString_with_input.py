b1c4e5cfc4f1aee68a53b37f0a748a19fa8e4397
daitran9628 <99699911+daitran9628@users.noreply.github.com>
2022-02-18 18:36:25 -0800

    change input to string

--- sortString_with_input.py
+++ sortString_with_input.py
@@ -1,5 +1,5 @@
-str1 = input("Enter your string: ")
-#str1 = "This is python"
+#str1 = input("Enter your string: ")
+str1 = "This is python for window"
 words = str1.split()
 words.reverse()
 print("sorted string ", words)
